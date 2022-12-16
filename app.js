399fc72b4f8978a6b35a02dc695e8ea03e149d49
andres101010 <andreschapu5295@gmail.com>
2022-12-15 22:19:19 -0300

    ruta para agregar

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', async (req,res)=>{
     const rows = await BD.query('SELECT * from peliculas')
     res.json(rows)
 })
+app.get('/crearPelicula', async (req,res)=>{
+    const response = await BD.query('INSERT INTO peliculas(nombre) VALUE("guerra de las galaxias")')
+    res.json(response)
+})
 
 app.listen(PORT);
 console.log("Corriendo en puerto", PORT);
