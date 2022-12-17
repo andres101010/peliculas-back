ffe5ebbf17917986faf5ba5cb878dda128ae8f0c
andres101010 <andreschapu5295@gmail.com>
2022-12-17 00:51:30 -0300

    nuevo1

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ export const PORT = process.env.PORT || 3001;
 export const BD_HOST = process.env.BD_HOST || 'localhost';
 export const BD_USER = process.env.BD_USER || 'root';
 export const BD_PASSWORD = process.env.BD_PASSWORD || 'root';
-export const BD_NAME = process.env.BD_NAME || 'products';
+export const BD_NAME = process.env.BD_NAME || 'gastos';
 export const BD_PORT = process.env.BD_PORT || 3306
